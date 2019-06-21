204f61499246581693b17522be72e78a0366415d
Vinícius Hoyer <vhoyer@live.com>
2019-06-21 00:28:03 -0300

    :white_check_mark: add test for false case

--- test/everyItemHasAllProperties.test.ts
+++ test/everyItemHasAllProperties.test.ts
@@ -24,17 +24,19 @@ describe('Validators > everyItemHasAllProperties', () => {
   ];
 
   describe('when a valid array is passed as value to be validated', () => {
-    it('returns true', () => {
-      const array = [{
-        prop1: 1,
-        prop2: 1,
-        prop3: 1,
-      }];
+    const array = [{
+      prop1: 1,
+      prop2: 1,
+      prop3: 1,
+    }];
 
-      expect(everyItemHasAllProperties(array, requiredProperties));
+    it('returns true', () => {
+      expect(everyItemHasAllProperties(array, requiredProperties)).toBe(true);
     });
 
     it('does not call console.error', () => {
+      everyItemHasAllProperties(array, requiredProperties);
+
       expect(consoleError).not.toBeCalled();
     });
   });
@@ -42,6 +44,31 @@ describe('Validators > everyItemHasAllProperties', () => {
   describe('when an empty array is passed to be validated', () => {
     it('returns true', () => {
       expect(everyItemHasAllProperties([], requiredProperties));
+    });
+  });
+
+  describe('when an invalid array is passed as value to be validated', () => {
+    const array = [{
+      prop1: 1,
+      prop2: 1,
+      prop3: 1,
+    }, {
+      prop1: 1,
+    }, {
+      // the third item is empty
+      // if this was the first object
+      // it should name all properties
+    }];
+
+    it('returns false', () => {
+      expect(everyItemHasAllProperties(array, requiredProperties)).toBe(false);
+    });
+
+    it('calls console.error with a message listing every missing property on the first inadequate item', () => {
+      everyItemHasAllProperties(array, requiredProperties);
+
+      expect(consoleError).toBeCalledWith(expect.stringContaining('prop3'));
+      expect(consoleError).toBeCalledWith(expect.stringContaining('prop3'));
     });
   });
 });
