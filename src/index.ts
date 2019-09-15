6c8146388e66d496c75353eceebff9fb05993c60
Vinícius Hoyer <vhoyer@live.com>
2019-09-15 19:00:48 -0300

    :boom: remove deprecated validators

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 export { default as everyItemOfArrayShouldHave } from './everyItemOfArrayShouldHave';
 export { default as objectShouldHave } from './objectShouldHave';
-// DEPRECATED
-export { default as everyItemHasAllProperties } from './everyItemHasAllProperties';
-export { default as hasAllProperties } from './hasAllProperties';
